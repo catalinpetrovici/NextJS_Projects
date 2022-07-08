f340a256a486f10985a564238c3720ce097f4a06
Petrovici Catalin-Cristian <petrovicicatalin24@yahoo.com>
2022-07-08 21:32:21 +0300

    dev.to Clone - initial commit

---
+++ dev.to Clone/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import Loader from '../components/Loader';
+
+export default function Home() {
+  return (
+    <div>
+      <Loader show />
+    </div>
+  );
+}
